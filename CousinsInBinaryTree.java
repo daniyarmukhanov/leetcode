2fa3570f1b2f1c86c86730e55752f29322c26e40
Daniyar <mukhanov.daniyar@gmail.com>
2020-05-07 16:24:10 +0600

    day 7

---
+++ CousinsInBinaryTree.java
@@ -0,0 +1,67 @@
+
+public class CousinsInBinaryTree {
+    public boolean isCousins(TreeNode root, int x, int y) {
+        int depX = findDepth(root, x, 0);
+        int depY = findDepth(root, y, 0);
+        if (depX != depY)
+            return false;
+        TreeNode parentX = findParent(root, x);
+        TreeNode parentY = findParent(root, y);
+        if (parentX == parentY)
+            return false;
+
+        return true;
+    }
+
+    private int findDepth(TreeNode root, int x, int depth) {
+        if (root != null) {
+            if (root.val == x)
+                return depth;
+            int left = findDepth(root.left, x, depth + 1);
+            int right = findDepth(root.right, x, depth + 1);
+            if (left != -1)
+                return left;
+            return right;
+        } else {
+            return -1;
+        }
+    }
+
+    private TreeNode findParent(TreeNode root, int x) {
+        if (root != null) {
+            if (root.left != null && root.left.val == x) {
+                return root;
+            }
+            if (root.right != null && root.right.val == x) {
+                return root;
+            }
+            TreeNode left = findParent(root.left, x);
+            TreeNode right = findParent(root.right, x);
+            if (left != null)
+                return left;
+            return right;
+        } else {
+            return null;
+        }
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
