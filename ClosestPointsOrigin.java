7d2fa6239db5c70a2347fadfb084ba329848ac2f
Daniyar <mukhanov.daniyar@gmail.com>
2020-05-31 02:41:30 +0600

    day 30

---
+++ ClosestPointsOrigin.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ClosestPointsOrigin {
+    public int[][] kClosest(int[][] points, int K) {
+        Arrays.sort(points, Comparator.comparing(p -> p[0] * p[0] + p[1] * p[1]));
+        return Arrays.copyOfRange(points, 0, K);
+    }
+}
