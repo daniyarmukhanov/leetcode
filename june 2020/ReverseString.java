71b8e6375e9b658504ea5d2ebb84c77e50b28f88
Daniyar <mukhanov.daniyar@gmail.com>
2020-06-04 15:34:10 +0600

    day 4

---
+++ june 2020/ReverseString.java
@@ -0,0 +1,9 @@
+public class ReverseString {
+    public void reverseString(char[] s) {
+        for (int i = 0; i < s.length / 2; i++) {
+            s[i] += s[s.length - 1 - i];
+            s[s.length - 1 - i] = (char) (s[i] - s[s.length - 1 - i]);
+            s[i] = (char) (s[i] - s[s.length - 1 - i]);
+        }
+    }
+}
