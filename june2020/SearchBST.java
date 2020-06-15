496598054e6f66771432cce62e7c328cff6247c8
Daniyar <mukhanov.daniyar@gmail.com>
2020-06-15 16:12:12 +0600

    day 15

---
+++ june2020/SearchBST.java
@@ -0,0 +1,20 @@
+package june2020;
+
+
+public class SearchBST {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null)
+            return null;
+
+        if (root.val == val)
+            return root;
+
+        if (root.val > val)
+            return searchBST(root.left, val);
+
+        else
+            return searchBST(root.right, val);
+
+
+    }
+}
